74f46807c22748951e610ea26b5655fe48a72c07
aashishg07 <ash.gtm07@gmail.com>
2023-11-22 13:49:20 +0545

    updated question number 1

--- task2/question1.py
+++ task2/question1.py
@@ -1,3 +1,10 @@
+"""
+1. The factorial of a non-negative integer N, denoted by N!, is the product of all positive
+integers less than or equal to N.   5! = 5 x 4 x 3 x 2 x 1 = 120
+
+"""
+
+
 number = int(input("Enter number: "))
 fact = 1
 
