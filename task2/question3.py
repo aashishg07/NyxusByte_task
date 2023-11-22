ea243e4eaabfe9ccc299896560673733d5573236
aashishg07 <ash.gtm07@gmail.com>
2023-11-22 13:47:38 +0545

    updated question number 3

--- task2/question3.py
+++ task2/question3.py
@@ -1,3 +1,28 @@
+'''
+
+3. Create a program for "Car Racing" with the following functionalities:
+    a. The initial state of the car is in the "stop" state.
+
+    b. Display a help message to the user explaining the available commands:
+    start -> start the car
+    stop -> stop the car
+    exit -> exit the program
+
+    c. Users are not allowed to enter the same command if the car is already in the
+    same state. For example:
+        If the car is in the "start" state and the user enters the start command
+        again, display "Car is already in start state."
+        Similarly, if the car is in the "stop" state and the user enters the stop
+        command again, display "Car is already in stop state."
+        
+    d. When the user enters exit, prompt them with "Are you sure you want to exit?"
+    If the user inputs "yes," the program should stop. If the user inputs anything
+    else, the program should continue.
+
+'''
+
+
+
 class Car:
     def __init__(self):
         self.car_started = False
@@ -36,8 +61,10 @@ while True:
     elif command == "stop":
         car.stop_car()
     elif command == "exit":
-        print("Exiting the program.........")
-        break
+        exit_command = input("Are you sure, you want to exit? (yes/no): ").lower()
+        if exit_command == "yes":
+            print("Exiting the program.........")
+            break
     else:
         print("Invalid command! Please follow the instruction.")
         car.help()
